bcc328883c2f6578fa2f094097def8f677a4975e
Shahroz Khan <shrozali@gmail.com>
2023-04-22 12:31:47 +0500

    version bump

--- buildSrc/src/main/kotlin/io.customer/android/Configurations.kt
+++ buildSrc/src/main/kotlin/io.customer/android/Configurations.kt
@@ -1,8 +1,8 @@
 package io.customer.android
 
 object Configurations {
-    const val compileSdk = 30
-    const val targetSdk = 30
+    const val compileSdk = 32
+    const val targetSdk = 32
     const val minSdk = 21
     const val artifactGroup = "io.customer.android"
 }
