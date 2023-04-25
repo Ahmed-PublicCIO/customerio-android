a8a9cf98246dc3087d8cee522dfb2d38782bf6c2
Shahroz Khan <shrozali@gmail.com>
2023-04-25 19:20:29 +0500

    storing anonymous profile identifier

--- sdk/src/main/java/io/customer/sdk/repository/preference/SitePreferenceRepository.kt
+++ sdk/src/main/java/io/customer/sdk/repository/preference/SitePreferenceRepository.kt
@@ -1,6 +1,7 @@
 package io.customer.sdk.repository.preference
 
 import android.content.Context
+import androidx.core.content.edit
 import io.customer.sdk.CustomerIOConfig
 import io.customer.sdk.extensions.getDate
 import io.customer.sdk.extensions.putDate
@@ -14,6 +15,9 @@ interface SitePreferenceRepository {
     fun saveAnonymousId(anonymousId: String)
     fun removeAnonymousId()
     fun getAnonymousId(): String?
+    fun saveAnonymousProfileId(anonymousProfileId: String)
+    fun removeAnonymousProfileId()
+    fun getAnonymousProfileId(): String?
 
     fun saveDeviceToken(token: String)
     fun getDeviceToken(): String?
@@ -35,60 +39,58 @@ internal class SitePreferenceRepositoryImpl(
     companion object {
         private const val KEY_IDENTIFIER = "identifier"
         private const val KEY_ANONYMOUS_ID = "anonymous_id"
+        private const val KEY_ANONYMOUS_PROFILE = "anonymous_profile_id"
         private const val KEY_DEVICE_TOKEN = "device_token"
         private const val KEY_HTTP_PAUSE_ENDS = "http_pause_ends"
     }
 
     override fun saveIdentifier(identifier: String) {
-        prefs.edit().putString(KEY_IDENTIFIER, identifier).apply()
+        prefs.edit { putString(KEY_IDENTIFIER, identifier) }
     }
 
     override fun removeIdentifier() {
-        prefs.edit().remove(KEY_IDENTIFIER).apply()
+        prefs.edit { remove(KEY_IDENTIFIER) }
     }
 
     override fun getIdentifier(): String? {
-        return try {
-            prefs.getString(KEY_IDENTIFIER, null)
-        } catch (e: Exception) {
-            null
-        }
+        return prefs.getString(KEY_IDENTIFIER, null)
     }
 
     override fun saveAnonymousId(anonymousId: String) {
-        prefs.edit().putString(KEY_ANONYMOUS_ID, anonymousId).apply()
+        prefs.edit { putString(KEY_ANONYMOUS_ID, anonymousId) }
     }
 
     override fun removeAnonymousId() {
-        prefs.edit().remove(KEY_ANONYMOUS_ID).apply()
+        prefs.edit { remove(KEY_ANONYMOUS_ID) }
     }
 
     override fun getAnonymousId(): String? {
-        return try {
-            prefs.getString(KEY_ANONYMOUS_ID, null)
-        } catch (e: Exception) {
-            null
-        }
+        return prefs.getString(KEY_ANONYMOUS_ID, null)
+    }
+
+    override fun saveAnonymousProfileId(anonymousProfileId: String) {
+        prefs.edit { putString(KEY_ANONYMOUS_PROFILE, anonymousProfileId) }
+    }
+
+    override fun removeAnonymousProfileId() {
+        prefs.edit { remove(KEY_ANONYMOUS_PROFILE) }
+    }
+
+    override fun getAnonymousProfileId(): String? {
+        return prefs.getString(KEY_ANONYMOUS_PROFILE, null)
     }
 
     override fun saveDeviceToken(token: String) {
-        prefs.edit().putString(KEY_DEVICE_TOKEN, token).apply()
+        prefs.edit { putString(KEY_DEVICE_TOKEN, token) }
     }
 
     override fun getDeviceToken(): String? {
-        return try {
-            prefs.getString(KEY_DEVICE_TOKEN, null)
-        } catch (e: Exception) {
-            null
-        }
+        return runCatching { prefs.getString(KEY_DEVICE_TOKEN, null) }.getOrNull()
     }
 
     override var httpRequestsPauseEnds: Date?
         get() = prefs.getDate(KEY_HTTP_PAUSE_ENDS)
         set(value) {
-            prefs.edit().apply {
-                putDate(KEY_HTTP_PAUSE_ENDS, value)
-                apply()
-            }
+            prefs.edit { putDate(KEY_HTTP_PAUSE_ENDS, value) }
         }
 }
